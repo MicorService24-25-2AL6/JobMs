3808e36649cde4073456946f75f565c29f8487fb
oumeima-ibnelfekih <oumeima.ibnelfekih@esprit.tn>
2025-05-07 14:01:21 +0100

    first commit

---
+++ src/main/java/esprit/alt/job/JobRepository.java
@@ -0,0 +1,8 @@
+package esprit.alt.job;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobRepository extends  JpaRepository<Job , Integer> {
+}
